31ac7557452334dc91b71eb165642405d3bcf185
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-11-20 14:59:26 -0700

    Connect Mongoose with MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = 'mongodb+srv://stephengroe:Hg8WydFcEswXiZX6@cluster0.rnyk07o.mongodb.net/local_library?retryWrites=true&w=majority';
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
